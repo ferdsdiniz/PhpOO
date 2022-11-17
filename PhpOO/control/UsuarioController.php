86205b5d71f650eb9fb7d6e53cbc915a75e5ca29
Fernanda <105492844+ferdsdiniz@users.noreply.github.com>
2022-11-17 19:31:06 -0300

    Add files via upload
    PhpOO - 3° semestre

---
+++ PhpOO/control/UsuarioController.php
@@ -0,0 +1,26 @@
+<?php
+
+include_once "../Conexao.php";
+include_once "../model/Usuario.php";
+include_once "../dao/UsuarioDAO.php";
+
+//Instancia as Classes
+
+$usuario = new Usuario();
+$usuariodao = new UsuarioDAO();
+
+//Pegar todos os dados passando via POST
+
+$form = filter_input_array(INPUT_POST);
+
+//Se a operação for gravar entra nessa condição
+if(isset($_POST['cadastrar'])){
+   $usuario->setNome($form['nome']);
+   $usuario->setSobrenome($form['sobrenome']);
+   $usuario->setIdade($form['idade']);
+   $usuario->setSexo($form['sexo']);  
+
+   $usuariodao->create($usuario);
+
+   header("Location: ../listar.php");
+}
