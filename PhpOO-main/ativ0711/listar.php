2318531740326c9564db0658118d4618a8220895
Fernanda <105492844+ferdsdiniz@users.noreply.github.com>
2022-11-21 19:34:38 -0300

    Add files via upload

---
+++ PhpOO-main/ativ0711/listar.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Listar Usuário</title>
+</head>
+    <body>
+        <div class="container">
+
+            <div>
+                <table class="table table-sm table-bordered table-hover">
+                    <thead>
+                        <tr>
+                            <th>ID: </th>
+                            <th>Nome: </th>
+                            <th>Sobrenome: </th>
+                            <th>Idade: </th>
+                            <th>Sexo: </th>
+                            <th>Ações: </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <td>01</td>
+                        <td>Fernanda</td>
+                        <td>Dias Diniz</td>
+                        <td>38</td>
+                        <td>Feminino </td>
+                        <td>
+                            <button class="btn btn-warning btn-sm">
+                                Editar
+                            </button>
+                            <button class="btn btn-danger btn-sm">
+                                Excluir
+                            </button>
+                        </td>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </body>
+</hmtl>
